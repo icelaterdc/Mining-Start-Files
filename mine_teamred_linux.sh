d45f928ecefac46ae727e308310c402fd9a86909
IceLater <120577848+icelaterdc@users.noreply.github.com>
2025-05-05 22:46:51 +0300

    Update mine_teamred_linux.sh

--- mine_teamred_linux.sh
+++ mine_teamred_linux.sh
@@ -8,6 +8,6 @@ while true; do
     --fanmin 50 \
     --fanmax 80 \
     --restart-on-error 10
-  echo "[ $(date '+%Y-%m-%d %H:%M:%S') ] Miner çöktü, 10 sn sonra yeniden başlatılıyor..."
+  echo "[ $(date '+%Y-%m-%d %H:%M:%S') ] Miner crashed, restarting after 10 seconds..."
   sleep 10
 done
