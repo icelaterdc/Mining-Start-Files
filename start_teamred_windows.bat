334300b000a4306a5cbd8e01d468e0174bc8cc29
IceLater <120577848+icelaterdc@users.noreply.github.com>
2025-05-05 22:43:16 +0300

    Create start_teamred_windows.bat

---
+++ start_teamred_windows.bat
@@ -0,0 +1,15 @@
+@echo off
+title TeamRedMiner ERGO Miner (Windows)
+:RESTART
+teamredminer.exe ^
+  -a ERGO ^
+  -o stratum+tcp://erg.2miners.com:8888 ^
+  -u YOUR_WALLET_ADDRESS.worker1 ^
+  -p x ^
+  --intensity 20 ^
+  --fanmin 50 ^
+  --fanmax 80 ^
+  --restart-on-error 10
+echo [ %DATE% %TIME% ] Miner crashed, restarting after 10 seconds...
+timeout /t 10 /nobreak >nul
+goto RESTART
